a6471944d25a9bb342755da6ff807256a7fb4b7f
Christopher <christopherqueiroz@hotmail.com>
2021-07-08 17:03:48 -0300

    Inclusão do enunciado

--- 01-exercicio.rb
+++ 01-exercicio.rb
@@ -12,3 +12,12 @@ puts "Eu pretendo comer "+((15*12)*60).to_s+" chocolates na vida."
 
 puts "Se sua idade é de 1232 milhões de segundos, ela em anos será:"
 puts "Convertendo a idade em anos, ela será de: "+(1232000000/(365*24*(60**2))).to_s+" anos."
+
+# Exercicio 01:
+# Escreva um programa que calcula e exiba na tela:
+# - quantas horas há em um ano?
+# - quantos minutos há em uma década?
+# - qual é a sua idade em segundos?
+# - quantos chocolates você pretende comer na vida?
+# Desafio:
+# - Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?
