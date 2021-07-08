d6eb5104f997d0b2c13a4125814e5484d3e0e731
Witchrist <christopherqueiroz@hotmail.com>
2021-07-08 14:45:41 -0300

    Exercício 01 - Nivelamento RD

---
+++ 01-exercicio.rb
@@ -0,0 +1,14 @@
+puts "Total de horas em um ano:"
+puts "Um ano tem "+(24*365).to_s+" horas."
+
+puts "Total de minutos em uma década:"
+puts "Uma década tem "+((24*60)*(365*10)).to_s+" minutos."
+
+puts "Minha idade em segundos é:"
+puts ((24*(60**2))*(365*23)).to_s+" segundos."
+
+puts "Quantos chocolates você pretende comer na vida?"
+puts "Eu pretendo comer "+((15*12)*60).to_s+" chocolates na vida."
+
+puts "Se sua idade é de 1232 milhões de segundos, ela em anos será:"
+puts "Convertendo a idade em anos, ela será de: "+(1232000000/(365*(24*(60**2)))).to_s+" anos."
