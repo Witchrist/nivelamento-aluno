03b188090c73a3ba0b9ab4adeac15248f833637d
Christopher <christopherqueiroz@hotmail.com>
2021-07-08 17:04:14 -0300

    Exercício 02 - Nivelamento RD

---
+++ 02-exercicio.rb
@@ -0,0 +1,28 @@
+horasAno = 24*365
+minutosAno = (24*60)*365
+segundosAno = (24*(60**2))*365
+chocolatesVida = (15*12)*60
+
+puts "Total de horas em um ano:"
+puts horasAno
+
+puts "Total de minutos em uma década:"
+puts minutosAno*10
+
+puts "Minha idade em segundos é:"
+puts segundosAno*23
+
+puts "Quantos chocolates você pretende comer na vida?"
+puts chocolatesVida
+
+puts "Se sua idade é de 1232 milhões de segundos, ela em anos será:"
+puts 1232000000/segundosAno
+
+
+
+# Usando variaveis, escreva um programa que calcula e exiba na tela:
+# - quantas horas há em um ano?
+# - quantos minutos há em uma década?
+# - qual é a sua idade em segundos?
+# - quantos chocolates você pretende comer na vida?
+# - Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?
