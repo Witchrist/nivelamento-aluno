1452f9821ae63b2e8fcc40985ae51a7d59f19688
Christopher <christopherqueiroz@hotmail.com>
2021-07-19 10:04:32 -0300

    Exclusão de puts

--- 10-exercicio-parte-4.rb
+++ 10-exercicio-parte-4.rb
@@ -15,7 +15,6 @@ while underscore<base
     end
     print externo+"_"+meio+"_"+externo  
     alguma = alguma+1 
-    puts underscore
 end
 
 # 4) Quer mais? Tente fazer uma letra V, usando asteriscos “*” e
